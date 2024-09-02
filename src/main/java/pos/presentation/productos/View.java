75beee649db7785f1d1743ba2d9188d3d3954f1c
Jose Mora Evans <josemoraevans02@gmail.com>
2024-09-01 22:16:46 -0600

    Hice el view.form del producto

---
+++ src/main/java/pos/presentation/productos/View.java
@@ -0,0 +1,24 @@
+package pos.presentation.productos;
+
+import javax.swing.*;
+
+public class View {
+    private JLabel Codigolbl;
+    private JTextField CodigoTxtfield;
+    private JLabel Preciolbl;
+    private JTextField PrecioTxtField;
+    private JButton guardarButton;
+    private JButton limpiarButton;
+    private JLabel Descripcionlbl;
+    private JLabel Unidad;
+    private JButton borrarButton;
+    private JTextField descripcionTxtField;
+    private JTextField unidadTxtField;
+    private JLabel Categorialbl;
+    private JComboBox categoriaComboBox;
+    private JTextField nombreTxtField;
+    private JLabel nombrelbl;
+    private JButton buscarButton;
+    private JButton reporteButton;
+    private JTable table1;
+}
